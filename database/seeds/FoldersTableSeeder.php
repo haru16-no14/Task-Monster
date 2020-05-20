fbf13592de6acfb0b7f46c4c0b3659dc0390c2d9
EC2 Default User <ec2-user@ip-172-31-91-137.ec2.internal>
2020-05-20 07:52:40 +0000

    folders

---
+++ database/seeds/FoldersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $titles = ['学校', '勉強', '遊び'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
